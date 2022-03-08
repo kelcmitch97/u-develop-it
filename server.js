0203bd2c5cfdee63f0c114de3f7d97de8f6eef3f
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-03-08 18:16:30 -0330

    Add query for candidates table

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// mysql2
+const mysql = require('mysql2');
+
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
+
+// Connet to Database
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        user: 'root',
+        password:'Lambeau#2021',
+        database: 'election'
+    },
+    console.log("Connected to the election database")
+);
+
 app.use(express.json());
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 // Default response for any other request (Not Found)
